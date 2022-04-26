4f5ad2d6804e42e25e8dc5c76874084974eecab4
Gary Butler <gary.butler@myob.com>
2022-04-26 16:34:53 +1000

    rename to javascript-sort

---
+++ javascript-sort/README.md
@@ -0,0 +1,41 @@
+# JavaScript Sort README
+
+Visual Studio Code extension to deeply sort the keys within a JavaScript object.
+
+## Features
+
+### Object Property Value Shorthand
+
+Object Property Value Shorthand keys can be sorted along with traditional key/value pairs.
+
+![Object Property Value Shorthand](images/object-property-value-shorthand.png)
+
+### Spread Operator
+
+Spread Operators are assumed to be the first keys in an object and will be left in their origianl position.
+
+![Spread Operators](images/spread-operator.png)
+
+### Single Line Comments
+
+Single line comments will be kept with the following key.
+
+![Single line comment](images/single-line-comment.png)
+
+## Requirements
+
+No requirements.
+
+## Known Issues
+
+Inline and multiline comments are not currently supported
+
+## Release Notes
+
+### 0.0.1
+
+Initial release
+
+---
+
+**Enjoy!**
--- javascript-sort/src/extension.ts
+++ javascript-sort/src/extension.ts
@@ -8,25 +8,18 @@ import { objToString } from './objToString';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations GBB, your extension "gazJsSort" is now active!');
-
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
-    'gbb-extensions.gazJsSort',
+    'extension.javaScript-sort',
     () => {
       try {
         const editor: any = vscode.window.activeTextEditor;
         const text = editor.document.getText(editor.selection);
 
         const orderedJson = deepSortJsObject(convertSelectedTextToJson(text));
-        console.log('-----orderedJson', orderedJson);
-
         const orderedJsonString = objToString(orderedJson);
-        console.log('---orderedJsonString', orderedJsonString);
 
         vscode.window.showInformationMessage('Sort complete');
 
